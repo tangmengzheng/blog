36e052b5b4e143941743c84831622c5824623b5e
tangmengzheng <tang_mengzheng@sina.com>
2015-08-08 18:03:34 +0800

    change text

---
+++ public/js/global.js
@@ -0,0 +1,11 @@
+// fancybox
+$(document).ready(function(){
+    $("#use_fancy").fancybox({
+        'width': '50%',
+        'height': '50%',
+        'autoScale': false,
+        'transitionIn': 'none',
+        'transitionOut': 'none',
+        'type': 'iframe'
+    });
+});
